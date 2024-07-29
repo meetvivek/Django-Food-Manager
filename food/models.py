f98269285db8d262bfebcf791ddf35da3fab2982
Vivek Kumar <kumarvivek.july28@gmail.com>
2024-07-29 15:27:20 +0530

    add Navbar

---
+++ food/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTne9zFARFBv8s504Akf4-Bdj2Dx6XIBWFHYQ&s")
